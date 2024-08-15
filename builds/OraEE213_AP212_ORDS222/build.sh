75b431e1db9cc5244c85696de56fc705a29ac669
Duane Dieterich <duane.dieterich+github@gmail.com>
2024-08-14 20:30:30 -0500

    Some Updates

--- builds/OraEE213_AP212_ORDS222/build.sh
+++ builds/OraEE213_AP212_ORDS222/build.sh
@@ -7,13 +7,19 @@
 #  -) Oracle REST Data Services 22.2
 #
 # Command Line Parameters
-#  -) 1 - PDB Password
-#  -) 2 - Version
+#  -) 1 - SYS Password
+#  -) 2 - User Password
+#  -) 3 - Version
 #
+
+SYS_PASS="${1}"
+USR_PASS="${2}"
+VERSION="${3}"
 
 # Copy stderr and stdout to log file
 exec &> >(tee build.log)
 
+########################################
 # Confirm starting location
 if ! ls "$(basename $0)"
 then
@@ -34,127 +40,19 @@ HOST_NAME_PORT='localhost:1521'
 CDB_NAME='EE213CDB'
 PDB_NAME='DEVPDB'
 ###
-PDB_PASS="${1}"
-VERSION="${2}"
-CDB_SYS="SYS/${PDB_PASS}@//${HOST_NAME_PORT}/${CDB_NAME} as sysdba"
-PDB_SYS="SYS/${PDB_PASS}@//${HOST_NAME_PORT}/${PDB_NAME} as sysdba"
-PDB_SYSTEM="SYSTEM/${PDB_PASS}@//${HOST_NAME_PORT}/${PDB_NAME}"
-PDB_WTP="WTP/WTP@//${HOST_NAME_PORT}/${PDB_NAME}"
-###
-echo ""
-echo "Capture Version"
-# 'https://github.com/DMSTEX/DMSTEX.git at f2c736d0cc6fd80d961414dcae37df2bed0d69e2 (Branch: main)'
-VERSION_NOTE="$(git config --get remote.origin.url 2>&1)" &&
-VERSION_NOTE="${VERSION_NOTE} at $(git rev-parse HEAD 2>&1)" &&
-VERSION_NOTE="${VERSION_NOTE} (Branch: $(git rev-parse --abbrev-ref HEAD 2>&1))"
-if [ $? = 0 ]
-then
-   echo "${VERSION_NOTE}" > "version.txt"
-else
-   echo "${VERSION}" > "version.txt"
-fi
+CDB_SYS="SYS/${SYS_PASS}@//${HOST_NAME_PORT}/${CDB_NAME} as sysdba"
+PDB_SYS="SYS/${SYS_PASS}@//${HOST_NAME_PORT}/${PDB_NAME} as sysdba"
+PDB_SYSTEM="SYSTEM/${SYS_PASS}@//${HOST_NAME_PORT}/${PDB_NAME}"
+PDB_WTP="WTP/26${USR_PASS}@//${HOST_NAME_PORT}/${PDB_NAME}"
 
 ########################################
-# Initialize the build
-echo ""
-echo "build_initialize.sql"
-sqlplus /nolog "@build_initialize.sql" "${PDB_NAME}" "${CDB_SYS}"
-retcd="${?}"
-if [ "${retcd}" != "0" ]
-then
-   echo "SQL*Plus returned ${retcd}.  Aborting"
-   exit "${retcd}"
-fi
+# Source the Build Functions
+. ../util/build_functions.sh
 
 ########################################
-function clear_log_files {
-   echo ""
-   echo "Clear old ${INSTALL_TYPE} logs"
-   rm -f "${INSTALL_TYPE}"/*.xml
-   rm -f "${INSTALL_TYPE}"/*.log
-   rm -f "${INSTALL_TYPE}"/*.bad
-   rm -f "${INSTALL_TYPE}"/*.dsc
-   rm -f "${INSTALL_TYPE}"/*/*.log
-   rm -f "${INSTALL_TYPE}"/*/*.bad
-   rm -f "${INSTALL_TYPE}"/*/*.dsc
-   }
-
-########################################
-function move_log_files {
-   echo ""
-   echo "Move Log Files to Build Folder"
-   mkdir -p "${BUILD_PATH}/${INSTALL_TYPE}" 2> /dev/null
-   ls *.xml *.log *.bad *.dsc 2>/dev/null |
-      while read FILE
-      do
-         mv -v "${FILE}" "${BUILD_PATH}/${INSTALL_TYPE}"
-      done
-   ls -F | grep '.*/$' |
-      while read DIR
-      do
-         mkdir -p "${BUILD_PATH}/${INSTALL_TYPE}/${DIR}" 2> /dev/null
-         ls "${DIR}"*.log "${DIR}"*.bad "${DIR}"*.dsc 2> /dev/null |
-            while read FILE
-            do
-               mv -v "${FILE}" "${BUILD_PATH}/${INSTALL_TYPE}/${DIR}"
-            done
-       done
-   }
-
-########################################
-function run_build {
-   INSTALL_TYPE="${1}"
-   clear_log_files
-   echo ""
-   echo "Move to ../../${INSTALL_TYPE}"
-   cd "../../${INSTALL_TYPE}"
-   echo ""
-   echo "${BUILD_PATH}/build.sql ${INSTALL_TYPE}"
-   sqlplus /nolog "@${BUILD_PATH}/build.sql" "${BUTIL_PATH}" "${PDB_SYS}" "${PDB_SYSTEM}" "${INSTALL_TYPE}" "${VERSION}"
-   retcd="${?}"
-   if [ "${retcd}" != "0" ]
-   then
-      echo "SQL*Plus returned ${retcd}.  Aborting"
-      exit "${retcd}"
-   fi
-   move_log_files
-   echo ""
-   echo "Move back to ${HOME_DIR}"
-   cd "${HOME_DIR}"
-   }
-
-########################################
-function setup_for_test {
-   echo ""
-   echo "Running ../util/setup_for_test.sql from ${PWD}"
-   sqlplus "${PDB_SYSTEM}" "@../util/setup_for_test.sql"
-   # Unit Testing Expects NO DB Links
-   #echo "Running ../util/setup_db_links.sql from ${PWD}"
-   #sqlplus "${PDB_WTP}" "@../util/setup_db_links.sql"
-   }
-
-########################################
-function run_core_test {
-   echo ""
-   echo "Running ../util/run_core_test.sql from ${PWD}"
-   sqlplus "${PDB_WTP}" "@../util/run_core_test.sql"
-   }
-
-########################################
-function run_junit_test {
-   echo ""
-   echo "Running ../util/run_junit_test.sql from ${PWD}"
-   sqlplus "${PDB_WTP}" "@../util/run_junit_test.sql"
-   }
-
-########################################
-function run_save_test {
-   echo ""
-   echo "Running ../util/run_save_test.sql from ${PWD}"
-   sqlplus "${PDB_WTP}" "@../util/run_save_test.sql"
-   }
-
-########################################
+clear_log_files
+capture_version
+build_init
 run_build 'wtpsrc'
 run_build 'wtptst'
 run_build 'wtpsav'
@@ -164,3 +62,4 @@ run_junit_test
 run_save_test
 run_build 'grbsrc'
 run_build 'wtpgrb'
+move_log_files
--- builds/util/run_build.sql
+++ builds/util/run_build.sql
@@ -6,8 +6,9 @@
 --   1 - BUTIL_PATH: Path to Build Utility Scripts
 --   2 - PDB_SYS: Connect String for SYS in the Pluggable Database
 --   3 - PDB_SYSTEM: Connect String for SYSTEM in the Pluggable Database
---   4 - INSTALL_TYPE: Installation Type
---   5 - APP_VERSION: Version for this application installation
+--   4 - PDB_PASSKEY: Part of the User/Schema Password Authentication
+--   5 - INSTALL_TYPE: Installation Type
+--   6 - APP_VERSION: Version for this application installation
 --
 
 WHENEVER SQLERROR EXIT SQL.SQLCODE
@@ -16,8 +17,9 @@ WHENEVER OSERROR EXIT
 define BUTIL_PATH="&1."
 define PDB_SYS="&2."
 define PDB_SYSTEM="&3."
-define INSTALL_TYPE="&4."
-define APP_VERSION="&5."
+define PDB_PASSKEY="&4."
+define INSTALL_TYPE="&5."
+define APP_VERSION="&6."
 
 set linesize 2499
 set trimspool on
@@ -25,9 +27,7 @@ set termout on
 set verify off
 set echo off
 set timing on
-
 @"&BUTIL_PATH./new_session.sql" "&PDB_SYS." "" ""
-
 set timing off
 @"install.sql" "&PDB_SYSTEM." "" ""
 
@@ -40,5 +40,15 @@ begin
    end if;
 end;
 /
+
+set linesize 123
+set trimspool on
+set termout on
+set verify off
+set echo off
+set timing on
+@"&BUTIL_PATH./new_session.sql" "&PDB_SYSTEM." "" ""
+set timing off
+@"set_user_authentication.sql" "&PDB_PASSKEY." "" ""
 
 exit
